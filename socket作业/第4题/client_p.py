af1f35b79cfdfb8d226b6bafdf9fba003addec8a
龙栉 <longzhi>
2019-04-04 08:56:26 +0800

    updated socket作

--- socket作业/第4题/client_p.py
+++ socket作业/第4题/client_p.py
@@ -1,22 +1,41 @@
 import socket
 from threading import Thread
 
+def sendmsg(client):
+    while 1:
+        msg = input('please input:')
+        try:
+
+            client.send(msg.encode())
+        except Exception:
+            client.close()
+            break
+
+def recvmsg(client):
+    while 1:
+        try:
+            msg = client.recv(65535)
+            if not msg:
+                break
+            print(msg.decode())
+        except Exception:
+            client.close()
+            break
 
 def connect(address):
     client = socket.socket()
     client.connect(address)
     print('.......server connected........')
 
-    while 1:
-        try:
-            msg = input('please input:')
-            client.send(msg.encode())
-        except Exception:
-            client.close()
-            break
+    send = Thread(target=sendmsg,args=(client,))
+    recv = Thread(target=recvmsg,args=(client,))
+    send.start()
+    recv.start()
+
+    send.join()
+    recv.join()
 
 
 if __name__ == "__main__":
-    address = ('127.0.0.1', 8080)
-    tk = Thread(target=connect,args=(address,))
-    tk.start()
+    address = ('127.0.0.1', 8087)
+    connect(address)
--- socket作业/第4题/server_p.py
+++ socket作业/第4题/server_p.py
@@ -1,30 +1,39 @@
 import socket
 from threading import Thread
+connlist = list()
 
+def server_recv(conn,addr):
+    while 1:
+
+        msg = conn.recv(65535)
+        if not msg:
+            break
+        showmsg = '来自{}的消息：{}'.format(addr[0], msg.decode())
+        print(showmsg)
+        for connmsg in connlist:
+            # print(connmsg)
+            connmsg.send(showmsg.encode())
 
 def server_connect(address):
     ss = socket.socket()
     ss.bind(address)
-    ss.listen(1)
+    ss.listen(100)
     print('<-------server started------>')
 
-    try:
-        conn, addr = ss.accept()
-        print('{} connected'.format(addr))
+    while 1:
+        try:
+            conn, addr = ss.accept()
+            connlist.append(conn)
+            print('{} connected'.format(addr))
+            tk = Thread(target=server_recv, args=(conn,addr))
+            tk.start()
 
-        while 1:
-
-            msg = conn.recv(65535)
-            if not msg:
-                break
-            print('来自{}的消息：{}'.format(addr[0], msg.decode()))
-    except Exception:
-        ss.close()
-        print('connection lost')
+        except Exception:
+            ss.close()
+            print('connection lost')
 
 
 
 if __name__ == "__main__":
-    address = ('127.0.0.1', 8080)
-    tk = Thread(target=server_connect, args=(address,))
-    tk.start()
+    address = ('127.0.0.1', 8087)
+    server_connect(address)
